73fcd6e5b96d89cd3b199fcc6d49eb527d2ad7ce
Priya <pvaidya56@gmail.com>
2020-03-26 02:30:09 -0700

    wrote all nctionality

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,32 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement >= 20){
+    return item;
+  }
+  else {
+    return { ...item, enhancement: item.enhancement + 1 };
+  }
+  
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15){
+    // item.durability = item.durability - 5;
+    item.durability-=5;
+  }
+  else if (item.enhancement >=15){
+    item.durability-=10;
+  }
+  if (item.enhancement > 16){
+    item.enhancement-=1;
+  }
+  return item;
 }
 
 function repair(item) {
-  return { ...item };
+
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
